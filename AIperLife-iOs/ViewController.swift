3d41acd84b1fa218cee402424f3193f8b5b4da0a
Eugenio <culurciello@gmail.com>
2017-02-25 08:41:10 -0500

    first commit
    menu works without thnets

---
+++ AIperLife-iOs/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AIperLife-iOs
+//
+//  Created by Eugenio Culurciello on 2/17/17.
+//  Copyright © 2017 Eugenio Culurciello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Actions
+
+
+}
+
